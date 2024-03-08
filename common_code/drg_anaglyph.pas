8c3302b264c3a42e0b3c362124a40a40bf11f193
prof-pguth <58475010+prof-pguth@users.noreply.github.com>
2024-03-07 19:16:41 -0500

    Add files via upload

--- common_code/drg_anaglyph.pas
+++ common_code/drg_anaglyph.pas
@@ -4,7 +4,7 @@ unit drg_anaglyph;
 { Part of MICRODEM GIS Program       }
 { PETMAR Trilobite Breeding Ranch    }
 { Released under the MIT Licences    }
-{ Copyright (c) 2023 Peter L. Guth   }
+{ Copyright (c) 2024 Peter L. Guth   }
 {____________________________________}
 
 
