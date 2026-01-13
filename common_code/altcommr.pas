fe15e94bc6fe0012750884df029a0a4ec3f04435
prof-pguth <58475010+prof-pguth@users.noreply.github.com>
2026-01-13 13:52:49 -0500

    Add files via upload

--- common_code/altcommr.pas
+++ common_code/altcommr.pas
@@ -4,7 +4,7 @@ unit AltCommR;
 { Part of MICRODEM GIS Program           }
 { PETMAR Trilobite Breeding Ranch        }
 { Released under the MIT Licences        }
-{ Copyright (c) 1986-2025 Peter L. Guth  }
+{ Copyright (c) 1986-2026 Peter L. Guth  }
 {________________________________________}
 
 {$I nevadia_defines.inc}
